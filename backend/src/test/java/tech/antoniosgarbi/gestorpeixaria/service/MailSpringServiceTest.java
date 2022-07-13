a78ee7781a30d0a3f180c65fee79b3e61b14e7b7
antoniosgarbi <antonio.sgarbi@hotmail.com>
2022-07-13 12:37:25 -0300

    add MailSpringServiceTest

---
+++ backend/src/test/java/tech/antoniosgarbi/gestorpeixaria/service/MailSpringServiceTest.java
@@ -0,0 +1,33 @@
+package tech.antoniosgarbi.gestorpeixaria.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import tech.antoniosgarbi.gestorpeixaria.service.impl.MailSpringServiceImpl;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class MailSpringServiceTest {
+    @Mock
+    private JavaMailSender emailSender;
+    @InjectMocks
+    private MailSpringServiceImpl underTest;
+
+    @Test
+    @DisplayName("Deve enviar email ao receber destinatário, assunto e corpo")
+    void sendText0() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("to");
+        message.setSubject("subject");
+        message.setText("body");
+
+        underTest.sendText("to", "subject", "body");
+        verify(emailSender).send(message);
+    }
+
+}
