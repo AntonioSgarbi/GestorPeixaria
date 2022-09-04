8ed7341970d7b69b88dd3ac4f5cf99e748f8dc7f
antoniosgarbi <antonio.sgarbi@hotmail.com>
2022-09-04 19:36:07 -0300

    update dependencies and prepare for spring native build

--- backend/src/main/java/tech/antoniosgarbi/gestorpeixaria/GestorPeixariaApplication.java
+++ backend/src/main/java/tech/antoniosgarbi/gestorpeixaria/GestorPeixariaApplication.java
@@ -1,8 +1,12 @@
 package tech.antoniosgarbi.gestorpeixaria;
 
+import org.hibernate.dialect.PostgreSQL10Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.nativex.hint.TypeHint;
 
+@TypeHint(types = PostgreSQL10Dialect.class, typeNames = "org.hibernate.dialect.PostgreSQL10Dialect")
 @SpringBootApplication
 public class GestorPeixariaApplication {
 
