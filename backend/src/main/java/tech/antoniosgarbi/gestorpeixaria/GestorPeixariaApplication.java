b99aef3cf3c19a255bfeb6a76ab97a1af7eaf42d
aokk <aokk@gft.com>
2023-06-12 18:37:12 -0300

    remove spring-native from project

--- backend/src/main/java/tech/antoniosgarbi/gestorpeixaria/GestorPeixariaApplication.java
+++ backend/src/main/java/tech/antoniosgarbi/gestorpeixaria/GestorPeixariaApplication.java
@@ -1,11 +1,8 @@
 package tech.antoniosgarbi.gestorpeixaria;
 
-import org.hibernate.dialect.PostgreSQL10Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.TypeHint;
 
-@TypeHint(types = PostgreSQL10Dialect.class, typeNames = "org.hibernate.dialect.PostgreSQL10Dialect")
 @SpringBootApplication
 public class GestorPeixariaApplication {
 
